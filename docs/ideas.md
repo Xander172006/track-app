55da7dd246f520dc814016f57026027e2b6fafd2
Xander172006 <xanderpoggenklaas@gmail.com>
2023-11-23 14:05:38 +0100

    improved the readme and hotfixes

--- docs/ideas.md
+++ docs/ideas.md
@@ -1,10 +1,12 @@
-use this to get salmon run rotations:
-https://splatoon3.ink/data/schedules.json
+## Ideas and inclusions for the app:
 
+Get the schedules of upcoming rotations through this api:
+```json
+    "schedules": "https://splatoon3.ink/data/schedules.json"
+```
 
-app functions:
-
-sql code:
+inclusion of personal stats to start out with:
+```sql
 UPDATE bosses SET steelheads = 3400, flyfishes = 1200, maws = 3300, stingers = 2900, steeleals = 1700, scrappers = 1717, 
 drizzlers = 1650, flippers = 1450, slamonlids = 1490, fishticks = 1530, bigshots = 1020 
 WHERE id = 1;
@@ -12,3 +14,4 @@ WHERE id = 1;
 UPDATE game_accounts
 SET bronzescales = 3112, silverscales = 945, goldscales = 210
 WHERE id = 1;
+```
--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -40,7 +40,7 @@ export default function Authenticated({ user, header, children }) {
                                         <span className="inline-flex rounded-md">
                                             <button
                                                 type="button"
-                                                className="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-white bg-black hover:text-gray-700 dark:hover:text-gray-300 transition ease-in-out duration-150"
+                                                className="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-white bg-black hover:text-gray-700 transition ease-in-out duration-150"
                                             >
                                                 {user && (
                                                     <>{user.name}</>
@@ -75,7 +75,7 @@ export default function Authenticated({ user, header, children }) {
                         <div className="-mr-2 flex items-center sm:hidden">
                             <button
                                 onClick={() => setShowingNavigationDropdown((previousState) => !previousState)}
-                                className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 dark:text-gray-500 hover:text-gray-500 dark:hover:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-900 focus:outline-none focus:bg-gray-100 dark:focus:bg-gray-900 focus:text-gray-500 dark:focus:text-gray-400 transition duration-150 ease-in-out"
+                                className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 dark:hover:text-gray-400 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out"
                             >
                                 <svg className="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
                                     <path
